c7aef0162b4b00bddf311b53a2c428620db72c87
Ömer Faruk Aydın <oemerfarukaydin97@gmail.com>
2023-06-10 15:50:29 +0300

    Update ikiEksen.ino

--- ikiEksen.ino
+++ ikiEksen.ino
@@ -11,16 +11,16 @@ int pos1 = 0, pos2 = 0, X_val, Y_val, SW_val, dly = 10, stp = 1;
 int min1 = 0, max1 = 180, min2 = 0, max2 = 180;
 
 void setup(){
-  myServo1.attach(myServo1);
-  myServo2.attach(myServo2);
+  myServo1.attach(SERVO1);
+  myServo2.attach(SERVO2);
   pinMode(SW_pin, INPUT_PULLUP); 
   digitalWrite(SW_pin, HIGH);
   pinMode(x_pin, INPUT); 
   pinMode(y_pin, INPUT);
 }
 void loop(){
-  X_val = analogRead(x_pin);
-  Y_val = analogRead(y_pin);
+  X_val = analogRead(X_pin);
+  Y_val = analogRead(Y_pin);
   SW_val = digitalRead(SW_pin);
   if (SW_val < 1) {
     if (dly == 5) dly = 10;
@@ -32,7 +32,7 @@ void loop(){
   if (X_val > 750 && pos1 < max1) pos1 += stp;
   if (Y_val < 250 && pos2 > min2) pos2 -= stp;
   if (Y_val > 750 && pos2 < max2) pos2 += stp;
-  myservo1.write(pos1);
-  myservo2.write(pos2);
+  myServo1.write(pos1);
+  myServo2.write(pos2);
   delay(dly);
 }
