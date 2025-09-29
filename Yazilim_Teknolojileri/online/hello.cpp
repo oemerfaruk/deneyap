a4743ac8b7436c2717e3f7f9ff8244bc89c2e245
oemerfaruk <oemerfarukaydin97@gmail.com>
2025-09-29 11:27:16 +0300

    Klasör güncellemesi | bazı uygulamaların eklenmesi

---
+++ Yazilim_Teknolojileri/online/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // input-output stream
+
+int main(){
+
+    std::cout << "Merhaba Dünya!" << std::endl;
+    
+    
+    return 0;
+}
---
+++ Yazilim_Teknolojileri/online/toplama.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+    cout << "A sayisini girin: ";
+    cin >> a;
+    cout << "B sayisini girin: ";
+    cin >> b;
+
+    /*
+    c = a + b;
+    cout << "Girdiginiz sayilarin toplami: " << c << endl;
+    */
+
+    cout << "Girdiginiz sayilarin toplami: " << a + b << endl;
+    return 0;
+}
