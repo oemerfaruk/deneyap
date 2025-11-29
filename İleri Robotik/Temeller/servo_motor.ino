78de1c4db639dc1edebe17016a63ea39ba1c3e7d
Ömer Faruk Aydın <oemerfarukaydin97@gmail.com>
2025-11-29 14:22:18 +0300

    Simplify servo control and remove serial output
    Removed serial output and simplified servo control.

--- İleri Robotik/Temeller/servo_motor.ino
+++ İleri Robotik/Temeller/servo_motor.ino
@@ -5,35 +5,11 @@
 
 Servo myservo1, myservo2;
 
-
 void setup(){
-    Serial.begin(115200);
-
     myservo1.attach(servopin1);
     myservo2.attach(servopin2);
-
-}2
+}
 void loop(){
-    // Bu uygulamada eğitmenler önce bir servo sonra ikinci servoyu takarak kademeli olarak ilerleyebilir
-
-    
-    for(int i = 0; i < 180; i++){
-        // Serial Ekran Servo Açısı Göster
-        Serial.print("Servo 1 Açı: ");
-        Serial.println(i);
-
-        // Servolara değer gönder
-        myservo1.write(i);
-    }
-
-    for(int i = 180; i > 0; i--){
-        // Serial Ekran Servo Açısı Göster
-        Serial.print("Servo 2 Açı: ");
-        Serial.println(i);
-
-        // Servolara değer gönder
-        myservo2.write(i);
-    }
-
-    delay(20);
+    myservo1.write(90);
+    myservo2.write(90);
 }
